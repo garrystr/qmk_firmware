4d91d0f8e32463b8283de08abd9144dc6d6fc5c4
garrystr <garrystr@gmail.com>
2023-01-03 20:30:04 +0000

    Another Update for checking

--- keyboards/spaceman/pancake/rev2/keymaps/garrystr/keymap.c
+++ keyboards/spaceman/pancake/rev2/keymaps/garrystr/keymap.c
@@ -5,13 +5,13 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layers {
-		_DVORAK
-		_QWERTY
-		_COLEMAK
-		_NUMS
-		_FUNCT
+		_DVORAK,
+		_QWERTY,
+		_COLEMAK,
+		_NUMS,
+		_FUNCT,
 		_ADJUST
-};  
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_ESC ,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-   KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT, \
-   SFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT , \
-   KC_SLSH,        KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT \
+   KC_ESC ,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+   KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT,
+   SFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT,
+   KC_SLSH,        KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT
 ),
 
 /* Colemak
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   /  | Ctrl | Alt  | GUI  | DEL  |Space |Adjust | Left| Down |  Up  |Right |   '  |
  * `-----------------------------------------------------------------------------------'
  */
-[_COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_ESC,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_MINS, KC_BSPC, \
-  KC_TAB,         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT, \
-  SFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT, \
-  KC_SLSH,        KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT   \
+[_COLEMAK] = LAYOUT_ortho_4x12(
+  KC_ESC,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_MINS, KC_BSPC,
+  KC_TAB,         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+  SFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT,
+  KC_SLSH,        KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT
 ),
 
 /* Dvorak
@@ -76,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   /  | Ctrl | Alt  | GUI  | DEL  |Space |Adjust | Left| Down |  Up  |Right |   '  |
  * `-----------------------------------------------------------------------------------'
  */
-[_DVORAK] = LAYOUT_ortho_4x12( \
-  KC_ESC,         KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_MINS, KC_BSPC, \
-  KC_TAB,         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
-  SFT_T(KC_CAPS), KC_Z,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_SCLN, KC_RSFT, \
-  KC_SLSH ,       KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT   \
+[_DVORAK] = LAYOUT_ortho_4x12(
+  KC_ESC,         KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_MINS, KC_BSPC,
+  KC_TAB,         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
+  SFT_T(KC_CAPS), KC_Z,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_SCLN, KC_RSFT,
+  KC_SLSH ,       KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT
 ),
 
 /* Numbers
@@ -94,11 +94,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   /  | Ctrl | Alt  | GUI  | DEL  |Space |Adjust | Left| Down |  Up  |Right |  ~   |
  * `-----------------------------------------------------------------------------------'
  */
-[_NUMS] = LAYOUT_ortho_4x12( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_GRV,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT, \
-  KC_LSFT, KC_NO,   KC_X,    KC_C,    KC_V,    KC_NO,   KC_NUBS, KC_NUHS, KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT, \
-  KC_SLSH ,KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TILD   \
+[_NUMS] = LAYOUT_ortho_4x12(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,  KC_GRV,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT,
+  KC_LSFT, KC_NO,   KC_X,    KC_C,    KC_V,    KC_NO,   KC_NUBS, KC_NUHS, KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT,
+  KC_SLSH ,KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TILD
 ),
 
 /* Function
@@ -112,11 +112,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Shift | Ctrl | Alt  | GUI  | DEL  |Space |Adjust| Left |  Up  | Down |Right |Shift |
  * `-----------------------------------------------------------------------------------'
  */
-[_FUNCT] = LAYOUT_ortho_4x12( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_BSPC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT, \
-  KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,   KC_R,    KC_M,    KC_D,    KC_NO,   KC_NO,   KC_DOT, \
-  KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RSFT  \
+[_FUNCT] = LAYOUT_ortho_4x12(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_BSPC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
+  KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,   KC_R,    KC_M,    KC_D,    KC_NO,   KC_NO,   KC_DOT,
+  KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,  KC_SPC,  ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RSFT
 ),
 
 /* Adjust
@@ -130,11 +130,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______,  QWERTY, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, DVORAK , FUNCT, _______, _______, _______, _______, _______,  _______, _______, \
-  _______, _______, _______, COLEMAK, _______, _______, NUMS,  _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+[_ADJUST] =  LAYOUT_ortho_4x12(
+  _______,  QWERTY, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, DVORAK , FUNCT, _______, _______, _______, _______, _______,  _______, _______,
+  _______, _______, _______, COLEMAK, _______, _______, NUMS,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 
